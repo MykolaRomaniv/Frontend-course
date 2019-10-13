58715ab3d2c301f56ba3d64c3eca40b8cd4897e5
Mykola Romaniv <mykolaromaniw@gmail.com>
2019-10-13 23:08:22 +0300

    done 6 task

---
+++ HM6/js/task2.js
@@ -0,0 +1,20 @@
+let a, b, c;
+
+a = parseFloat(prompt("Enter lenth of side A"));
+b = parseFloat(prompt("Enter lenth of side B"));
+c = parseFloat(prompt("Enter lenth of side C"));
+
+if ((a + b > c) && (a + c > b) && (b + c > a)) {
+    switch (true) {
+        case a == b && b == c:
+            alert("Equivalent triangle");
+            break;
+        case a == b || b == c || a == c:
+            alert("Isosceles triangle");
+            break;
+        default:
+            alert("Normal triangle");
+    }
+} else {
+    alert("Triangle doesn’t exist");
+}
