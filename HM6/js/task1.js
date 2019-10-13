f663d023a2fe9f8ae550c69f1fab04e39739e205
Mykola Romaniv <mykolaromaniw@gmail.com>
2019-10-13 23:40:21 +0300

    refator to correspond eslint warnings

--- HM6/js/task1.js
+++ HM6/js/task1.js
@@ -2,19 +2,19 @@ let a = [],
     b = [],
     c = [];
 
-a.x = parseFloat(prompt("Enter Ax numeric values"));
-a.y = parseFloat(prompt("Enter Ay numeric values"));
-b.x = parseFloat(prompt("Enter Bx numeric values"));
-b.y = parseFloat(prompt("Enter By numeric values"));
-c.x = parseFloat(prompt("Enter Cx numeric values"));
-c.y = parseFloat(prompt("Enter Cy numeric values"));
+a.x = parseFloat(prompt('Enter Ax numeric values'));
+a.y = parseFloat(prompt('Enter Ay numeric values'));
+b.x = parseFloat(prompt('Enter Bx numeric values'));
+b.y = parseFloat(prompt('Enter By numeric values'));
+c.x = parseFloat(prompt('Enter Cx numeric values'));
+c.y = parseFloat(prompt('Enter Cy numeric values'));
 
-if ((c.x - a.x) / (b.x - a.x) == (c.y - a.y) / (b.y - a.y)) {
-    if (c.x == (a.x + b.x) / 2 && c.y == (a.y + b.y) / 2) {
-        alert("Point is in center");
+if ((c.x - a.x) / (b.x - a.x) === (c.y - a.y) / (b.y - a.y)) {
+    if (c.x === (a.x + b.x) / 2 && c.y === (a.y + b.y) / 2) {
+        alert('Point is in center');
     } else {
-        alert("Points are on the same line");
+        alert('Points are on the same line');
     }
 } else {
-    alert("Points are not on the same line");
+    alert('Points are not on the same line');
 }
--- HM6/js/task2.js
+++ HM6/js/task2.js
@@ -1,20 +1,20 @@
 let a, b, c;
 
-a = parseFloat(prompt("Enter lenth of side A"));
-b = parseFloat(prompt("Enter lenth of side B"));
-c = parseFloat(prompt("Enter lenth of side C"));
+a = parseFloat(prompt('Enter lenth of side A'));
+b = parseFloat(prompt('Enter lenth of side B'));
+c = parseFloat(prompt('Enter lenth of side C'));
 
-if ((a + b > c) && (a + c > b) && (b + c > a)) {
+if (a + b > c && a + c > b && b + c > a) {
     switch (true) {
-        case a == b && b == c:
-            alert("Equivalent triangle");
+        case a === b && b === c:
+            alert('Equivalent triangle');
             break;
-        case a == b || b == c || a == c:
-            alert("Isosceles triangle");
+        case a === b || b === c || a === c:
+            alert('Isosceles triangle');
             break;
         default:
-            alert("Normal triangle");
+            alert('Normal triangle');
     }
 } else {
-    alert("Triangle doesn’t exist");
+    alert('Triangle doesn’t exist');
 }
