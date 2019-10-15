09d36a996b5fc5b80e33b32abe9f22320373f67e
Mykola Romaniv <mykolaromaniw@gmail.com>
2019-10-15 19:53:04 +0300

    done 8/10 tasks

--- HW9/index.js
+++ HW9/index.js
@@ -28,7 +28,6 @@ function findTypes() {
     }
     return outObj;
 }
-
 // console.log(findTypes(null, 5, 'hello', 'name', -454645, undefined, {age:15, name: 'name'}));
 
 function executeforEach(arr, f) {
@@ -36,29 +35,101 @@ function executeforEach(arr, f) {
         f(elem);
     });
 }
-
 // executeforEach([1,2,3], function(el) { console.log(el) });
 
 function mapArray(arr, f) { //TODO use executeforEach()
-    for(let i in arr) {
+    for (let i in arr) {
         arr[i] = f(arr[i]);
     }
     return arr;
 }
-
 // console.log(mapArray([2, 5, 8], function (el) {
 //     return el + 3
 // }));
 
-function filterArray(arr, f) {//TODO use executeforEach()
+function filterArray(arr, f) { //TODO use executeforEach()
     let newArr = [];
     arr.forEach(elem => {
-        if(f(elem))
-        {
+        if (f(elem)) {
             newArr.push(elem);
         }
     });
     return newArr;
 }
-
 // console.log( filterArray([2, 5, 8], function(el) { return el > 3 }));
+
+function showFormattedDate(d) {
+    return 'Date: ' + d.toDateString().slice(4);
+}
+// console.log(showFormattedDate(new Date('2019-01-27T01:10:00')) ); //returns ‘Date: Jan 27 2019’
+
+function canConvertToDate(str) {
+    return Date.parse(str) ? true : false;
+}
+// console.log(canConvertToDate('2016-13-18T00:00:00')); // false
+// console.log(canConvertToDate('2016-03-18T00:00:00')); // true
+
+function daysBetween(d1, d2) {
+
+}
+// console.log(daysBetween(new Date('2016-03-18T00:00:00'), new Date('2016-04-19T00:00:00')));  // 32
+
+function getAmountOfAdultPeople(data) {
+
+}
+
+let DATA = [{
+        "_id": "5b5e3168c6bf40f2c1235cd6",
+        "index": 0,
+        " birthday ": '2016-03-18T00:00:00',
+        "eyeColor": "green",
+        "name": "Stein",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5b5e3168e328c0d72e4f27d8",
+        "index": 1,
+        " birthday ": '1991-02-11T00:00:00',
+        "eyeColor": "blue",
+        "name": "Cortez",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5b5e3168cc79132b631c666a",
+        "index": 2,
+        " birthday ": '1984-04-17T00:00:00',
+        "eyeColor": "blue",
+        "name": "Suzette",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5b5e31682093adcc6cd0dde5",
+        "index": 3,
+        " birthday ": '1994-04-17T00:00:00',
+        "eyeColor": "green",
+        "name": "George",
+        "favoriteFruit": "banana"
+    }
+];
+// console.log(getAmountOfAdultPeople(DATA)); // returns 3;
+
+function keys(obj) {
+    let outArr = [];
+    // for (let el of Object.keys(obj)) {
+    //     outArr.push(el);
+    // }
+    for (let i in obj) {
+        outArr.push(i);
+    }
+    return outArr;
+}
+// console.log(keys({keyOne: 1, keyTwo: 2, keyThree: 3})); // returns [“keyOne”, “keyTwo”, “keyThree”]
+
+function values(obj) {
+    let outArr = [];
+    for (let i in obj) {
+        outArr.push(obj[i]);
+    }
+    return outArr;
+}
+// console.log(values({keyOne: 1, keyTwo: 2, keyThree: 3})); // returns [1, 2, 3]
