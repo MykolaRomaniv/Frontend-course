3e76d37dee7f908528ae68281c57c21d4441d8bf
Mykola Romaniv <mykolaromaniw@gmail.com>
2019-10-20 23:25:27 +0300

    done add function

--- HW11/src/app.js
+++ HW11/src/app.js
@@ -1,3 +1,18 @@
-let rootNode = document.getElementById("root");
+//let rootNode = document.getElementById("root");
+function liInner(innerText) {
+    return `<span class="item">
+    <i class="material-icons md-blue">check_box_outline_blank</i>
+    <span class="text"> ` + innerText +` </span>
+    <i class="material-icons md-36 md-blue">create</i>
+</span>
+<i class="material-icons md-36 md-blue">delete</i>`;
+}
 
-// Your code goes here
+let itemToAdd; //read from input
+
+itemToAdd = 'Find the cat';
+
+let list = document.querySelector('ul.list');
+let li = document.createElement('li');
+li.innerHTML = liInner(itemToAdd);
+list.appendChild(li);
